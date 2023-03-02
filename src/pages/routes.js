ae332b2f170f20956e9d005875b83a592ace98a0
a.lamkov <a.lamkov@vk.team>
2023-03-02 11:36:06 +0500

    Feat: add routing

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,10 +1,39 @@
 const routes = {
   '/': 'ChatPage',
+  '/chats': 'ChatPage',
   '/sign-in': 'SignInPage',
+  '/sign-up': 'SignUpPage',
+  '/profile': 'ProfilePage',
+  '/unknown-error': 'Error5XXPage',
   '*': 'Error404',
 }
 
 const getCurrentPage = () => routes[location.pathname] || routes['*']
 
-export { getCurrentPage }
+const initRouter = (callback) => {
+  switch (getCurrentPage()) {
+    case 'ChatPage': {
+      import('./ChatPage/template.ejs').then(callback)
+      break
+    }
+    case 'SignInPage': {
+      import('./SignInPage/template.ejs').then(callback)
+      break
+    }
+    case 'SignUpPage': {
+      import('./SignUpPage/template.ejs').then(callback)
+      break
+    }
+    case 'ProfilePage': {
+      import('./ProfilePage/template.ejs').then(callback)
+      break
+    }
+    default: {
+      import('./Error404Page/template.ejs').then(callback)
+      break
+    }
+  }
+}
+
+export { getCurrentPage, initRouter }
 export default routes
